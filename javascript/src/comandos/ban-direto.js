dd838766b3d5be195215d8e56d425e2e098978e7
Dev_Josué <92229705+JosueHTML2@users.noreply.github.com>
2022-03-15 18:08:01 +0000

    O comando ban-direto.js.
    Adicionando.

--- javascript/src/comandos/ban-direto.js
+++ javascript/src/comandos/ban-direto.js
@@ -2,3 +2,16 @@
 //Exemplo: !ban <@user> <motivo>
 //Linguagem usada: js
 //Author: aquelemesmoojack#4306
+//Contribuidor JosueHTML
+
+module.exports.run = (client, message) => {
+   const memberId = message.content.split(/ +/)[0] || message.mentions.users.first(); // Se tiver o id na mensagem vai ser se não vai ser a menção
+   const { guild } = message;
+  //Banindo
+  guilds.members.cache.get(memberId).ban({
+    days: 3,
+    reason: message.content.split(/ +/)[1] 
+  });
+  
+  
+}
